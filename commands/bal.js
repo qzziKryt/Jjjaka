23c760c78eabc6be2e68718a59f5f571a7719cec
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-08 18:03:32 +0200

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -4,18 +4,18 @@ exports.execute = async (client, message, args) => {
     let user = message.mentions.users.first() || message.author;
     let userBalance = client.eco.fetchMoney(user.id);
     const embed = new MessageEmbed()
-        .setTitle(`Balance`)
-        .addField(`User`, `<@${userBalance.user}>`)
-        .addField(`Balance`, `${userBalance.amount} 💸`)
-        .addField(`Position`, userBalance.position)
-        .setColor("RANDOM")
-        .setThumbnail(user.displayAvatarURL)
+        .setTitle(`Информация`)
+        .addField(`Участник`, `<@${userBalance.user}>`)
+        .addField(`Писька`, `${userBalance.amount} см`)
+        .addField(`Место`, userBalance.position)
+        .setColor("#rrggbb")
+        .setThumbnail(user.displayAvatarURL())
         .setTimestamp();
     return message.channel.send(embed);
 }
 
 exports.help = {
-    name: "bal",
-    aliases: ["money", "credits", "balance"],
-    usage: `bal`
+    name: "balance",
+    aliases: ["info", "bal"],
+    usage: `bal @user`
 }
