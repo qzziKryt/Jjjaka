2fb1cd25b30172a4a06e762996c0d909ac0a181e
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-08 18:09:25 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,15 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
+    let user = message.mentions.users.first() || message.author;
     const embed = new MessageEmbed()
-        .setAuthor("Commands")
-        .setTitle("Make Sure To Check Out This Channel For More Such Bots")
-        .setURL("https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ")
-        .setDescription(`Total Commands: ${client.commands.size}`)
-        .setColor("BLURPLE")
+        .setTitle(`Команды`)
+        .setDescription(`Всего команд: ${client.commands.size}`)
+        .setColor("#rrggbb")
         .setTimestamp()
-        .setThumbnail(client.user.displayAvatarURL)
-        .setFooter(message.author.tag, message.author.displayAvatarURL);
+        .setFooter(message.author.tag, message.author.displayAvatarURL());
     client.commands.forEach(cmd => {
-        embed.addField(`${cmd.help.name}`, `Aliases: ${cmd.help.aliases.join(", ") || "None"}\nUsage: \`${client.prefix}${cmd.help.usage}\``, true);
+        embed.addField(`${cmd.help.name}`, `Псевдонимы: ${cmd.help.aliases.join(", ") || "None"}\nПрименение: \`${client.prefix}${cmd.help.usage}\``, true);
     });
     return message.channel.send(embed);
 }
