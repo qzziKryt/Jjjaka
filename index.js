b89d17b2ccd3d151c33ad11990f2e99d1f34d875
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-08 19:11:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const keep_alive = require("./keep_alive.js");
  const client = new Discord.Client({ disableMentions: 'everyone' });
 const Eco = require("quick.eco");
 client.eco = new Eco.Manager(); // quick.eco
